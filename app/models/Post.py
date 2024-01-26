882d74366bac34106c9abcea4b9fe1242181b2f8
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2024-01-26 16:47:30 -0500

    Fixed a bug in the post.py file

--- app/models/Post.py
+++ app/models/Post.py
@@ -14,7 +14,7 @@ class Post(Base):
   created_at = Column(DateTime, default=datetime.now)
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
   vote_count = column_property(
-    select([func.count(Vote.id)]).where(Vote.post_id == id)
+    select(func.count(Vote.id)).where(Vote.post_id == id)
   )
   user = relationship("User")
   comments = relationship("Comment", cascade="all,delete")
